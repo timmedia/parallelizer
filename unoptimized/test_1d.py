ab79f4cf9adee7eb1ae6e3f7ec261d0384225f22
Tim Mutkala <tim.mutkala@afry.com>
2024-09-11 14:19:02 +0200

    feat: write tests

---
+++ unoptimized/test_1d.py
@@ -0,0 +1,24 @@
+from mpi4py import MPI
+import numpy as np
+from time import sleep
+
+# Initialize MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+global_array = np.arange(size * 5 + 1)
+local_array = global_array[rank*5:(rank+1)*5+1]
+if rank == 0:
+    print(global_array)
+sleep(0.2)
+print(f"{rank=} {local_array=}")
+
+recvbuf = None
+if rank == 0:
+    recvbuf = np.empty((size, 6), dtype=int)
+
+comm.Gather(local_array, recvbuf, root=0)
+
+if rank == 0:
+    print(f"{recvbuf.flatten()=}")
---
+++ unoptimized/test_gather_2d.py
@@ -0,0 +1,29 @@
+import numpy as np
+from mpi4py import MPI
+from time import sleep
+
+from nmwc_model.parallel import exchange_borders_2d, gather_2d
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+rank_size = comm.Get_size()
+
+gathered_data = np.arange(128).reshape((16, -1))
+nb = 2
+nx = 3
+if rank == 0:
+    print(f"data:\n{gathered_data}\n")
+
+sleep(0.1 * rank)
+
+rank_data = gathered_data[rank*(nx):(rank+1)*nx + 2 * nb, :]
+rank_data[:nb, :] = 0
+rank_data[-nb:, :] = 0
+print(f"{rank=}\n{rank_data}\n")
+
+output = np.zeros_like(gathered_data)
+
+gather_2d(rank_data, output, nx, slice(nb, -nb))
+
+if rank == 0:
+    print(f"{output}")
